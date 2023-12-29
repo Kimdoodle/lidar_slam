4959cbd4a69dc9b9bef1c9eae91fa27acd4f0088
KimDoodle <doodle@dgu.ac.kr>
2023-12-29 16:47:12 +0900

    Init: Add default SDK

---
+++ catkin_ws/build/rplidar_ros/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/doodle/바탕화면/LiDAR_Study/catkin_ws/src/rplidar_ros
+# Build directory: /home/doodle/바탕화면/LiDAR_Study/catkin_ws/build/rplidar_ros
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
