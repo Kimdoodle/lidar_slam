bccc238783d4e1c6078252d32f4db64223a94335
Kimdoodle <doodle@dgu.ac.kr>
2024-04-07 01:01:52 +0900

    Feat: GUI for preprocessing 지도학습용 데이터 선별 그래픽 인터페이스 추가

--- src/scanFile.py
+++ src/scanFile.py
@@ -2,6 +2,8 @@
 import os
 import re
 
+import numpy as np
+
 
 # 로그 파일을 스캔하여 좌표 데이터 반환
 def load(path: str) -> list:
@@ -19,11 +21,38 @@ def load(path: str) -> list:
             data_list = []
             for item in data:
                 ele = tuple(map(float, item.split(',')))
-                # 근거리 데이터는 장애물로 인식하여 포함하지 않음
-                if ele[2] > 500:
-                    data_list.append(ele[1:])
+                data_list.append(ele[1:])
             fileData.append(data_list)
 
         return fileData
     except Exception as e:
         print(e)
+
+
+'''
+csv파일 column
+angleDiff / distanceDiff / distanceFromCenterDiff
+'''
+
+
+# 로그데이터를 계산하여 csv파일로 저장
+def text2Csv(dataList: list):
+    # data는 각 파일별 데이터를 삽입한 list의 형태
+    for data in dataList:
+        length = len(data)
+        info = []
+        for i in range(length):
+            dot1 = data[i]
+            dot2 = data[(i + 1) % length]
+            angleDiff = np.abs(dot1[1] - dot2[1])
+            distanceFromCenterDiff = np.abs(dot1[2] - dot2[2])
+            dot1X, dot1Y = calXY(dot1[1], dot1[2])
+            dot2X, dot2Y = calXY(dot2[1], dot2[2])
+            distanceDiff = np.sqrt((dot1X - dot2X) ** 2 + (dot1Y - dot2Y) ** 2)
+            info.append((angleDiff, distanceDiff, distanceFromCenterDiff))
+
+
+def calXY(angle, dist):
+    x = dist * np.cos(np.radians(angle))
+    y = dist * np.sin(np.radians(angle))
+    return x, y
