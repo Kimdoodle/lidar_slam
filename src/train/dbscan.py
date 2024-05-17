3816337f650b6c88b7ff9faedfc8db55d916b2e8
KimDoodle <doodle@dgu.ac.kr>
2024-05-18 01:49:33 +0900

    Docs: Todo file

--- src/train/dbscan.py
+++ src/train/dbscan.py
@@ -1,10 +1,11 @@
 import sys
 import os
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 import data
 import pandas as pd
 from sklearn.cluster import DBSCAN
-import  matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import time
 
@@ -15,7 +16,8 @@ def dataProcess(dirPath: str):
     for filename in os.listdir(dirPath):
         filepath = os.path.join(dirPath, filename)
         data.convertCSV(filepath)
-    
+
+
 def dbscan(dirPath: str, dirPath2: str):
     minPts = 4
     eps = None
@@ -31,7 +33,7 @@ def dbscan(dirPath: str, dirPath2: str):
         if filename.endswith('.csv'):
             filepath = os.path.join(dirPath, filename)
             data = pd.read_csv(filepath)
-            
+
             # 데이터가 100개 미만인 경우 클러스터링을 하지 않음
             if len(data) < 100:
                 continue
@@ -42,15 +44,15 @@ def dbscan(dirPath: str, dirPath2: str):
             # DBSCAN 실행
             db = DBSCAN(eps=eps, min_samples=minPts).fit(data)
             labels = db.labels_
-            
+
             # 클러스터링 결과를 새로운 컬럼에 저장
             data['Cluster'] = labels
-            
+
             # 결과를 새 파일로 저장
             result_path = os.path.join(dirPath, filename.replace('.csv', '_clustered.csv'))
             data.to_csv(result_path, index=False)
             print(f"Clustering completed and saved to {result_path}")
-            
+
             # 시각화
             plt.figure(figsize=(10, 6))
             plt.scatter(data['x'][labels == -1], data['y'][labels == -1], c='red', label='Outlier')
@@ -59,7 +61,7 @@ def dbscan(dirPath: str, dirPath2: str):
             plt.xlabel('X')
             plt.ylabel('Y')
             plt.legend()
-            
+
             # 그림 파일로 저장
             plot_path = os.path.join(dirPath2, filename.replace('.csv', '.png'))
             print(plot_path)
@@ -67,6 +69,7 @@ def dbscan(dirPath: str, dirPath2: str):
             plt.savefig(plot_path)
             plt.close()
             print(f"Plot saved to {plot_path}")
+
 
 if __name__ == '__main__':
     # dataProcess('./log/2024-05-05')
